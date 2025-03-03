d4e9a5126bf6739eb2f96ed70742782c29b80c4b
Alejandro <108953308+vendieta@users.noreply.github.com>
2025-03-03 20:44:35 +0000

    smalls changes

--- components/TEST/UserCard.tsx
+++ components/TEST/UserCard.tsx
@@ -27,7 +27,7 @@ interface DataProps {
 
 const UserCard: React.FC<DataProps> = ({element}) => {
   return (
-    <Link href={`../(serviceScreen)/${element}`} style={styles.link} >
+    <Link href={`../${element}`} style={styles.link} >
       <View style={styles.card}>
         {/* User Info and Image */}
         <View style={styles.userInfoContainer}>
