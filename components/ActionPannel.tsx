f8572370fd24c857b3ee2477c6c8131f31596e4b
Ariel Arias <126928333+ArielAT123@users.noreply.github.com>
2025-04-27 23:24:57 -0500

    add context themed in AcctionPannel

--- components/ActionPannel.tsx
+++ components/ActionPannel.tsx
@@ -1,151 +1,135 @@
 import { useState } from "react";
-import { View , StyleSheet , Text, Pressable, TouchableOpacity } from "react-native";
-import { useRouter } from "expo-router"; 
+import { View, StyleSheet, Text, Pressable, TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
+import { useTheme } from "../components/Themed/ContextTheme";
 
 interface Coordinate {
-  latitude : number;
-  longitude : number;
-};
-
+  latitude: number;
+  longitude: number;
+}
 
 interface Props {
-  region : Coordinate
+  region: Coordinate;
   confCoordinate: (data: "startPoint" | "endPoint" | "menu") => void;
 }
 
-
-export default function ActionPannel (data : Props){
+export default function ActionPannel(data: Props) {
   const router = useRouter();
-  const [ controler , setControler ] = useState('startPoint')
+  const { theme } = useTheme();
+  const [controler, setControler] = useState('startPoint');
   
-  // estas constantes van a alvergar las coordenadas
-  const [ startPoint , setStartPoint ] = useState<string>('     ')
-  const [ endPoint , setEndPoint ] = useState<string>('     ')
+  const [startPoint, setStartPoint] = useState<string>('     ');
+  const [endPoint, setEndPoint] = useState<string>('     ');
   
-  const pointStart = (x : Coordinate) => {
-    data.confCoordinate('startPoint')
-    setControler('menu')
-    setStartPoint(data.region.longitude.toString())
-  }
+  const pointStart = (x: Coordinate) => {
+    data.confCoordinate('startPoint');
+    setControler('menu');
+    setStartPoint(data.region.longitude.toString());
+  };
   
-  const pointEnd = (x : Coordinate) => {
-    data.confCoordinate('endPoint')
-    setControler('menu')
-    setEndPoint(data.region.longitude.toString())
-  }
+  const pointEnd = (x: Coordinate) => {
+    data.confCoordinate('endPoint');
+    setControler('menu');
+    setEndPoint(data.region.longitude.toString());
+  };
 
   const send = () => {
     router.push("/send");
-  } 
-
-  // const plus = (x : Coordinate) => {
-  //   data.confCoordinate('add')
-    
-  // }
-  
-
+  };
 
   return (
-    <View style = {styles.container}>
+    <View style={[styles.container, { backgroundColor: theme.cardBackground }]}>
       {controler === 'startPoint' ? 
         <>
-          <Text style = {styles.text}>Punto de origen</Text>
-          <Text style = {styles.ubi}>{data.region.longitude}</Text>
-          {/* aqui va el input del lugar de partida y envia los datos a la api con el algoritmo 
-            aqui podriamos usar la api de google la cual me da el nombre de la ubicacion por las coodenadas*/}
-          {/* <Text style = { styles.text } >Punto de destino</Text>
-          <Text style = {styles.ubi}>{data.pointEnd}</Text> */}
-    
-          {/* aqui va el inpunt del lugar de destino y envia los dato a la api con el algoritmo
-            aqui tambien hacemos lo mismo de arriba*/}
+          <Text style={[styles.text, { color: theme.text }]}>Punto de origen</Text>
+          <Text style={[styles.ubi, { backgroundColor: theme.subtleBackground }, { color: theme.text }]}>
+            {data.region.longitude}
+          </Text>
     
           <Pressable onPress={() => pointStart(data.region)}>
-            <Text style = {styles.button} >Confirmar</Text>
+            <Text style={[styles.button, { backgroundColor: theme.accent }]}>
+              Confirmar
+            </Text>
           </Pressable>
-          {/* Aqui debe estar el boton para enviar los datos a la api */}
         </> : controler === 'menu' ? 
         <>
-          <Text>Tu ubicacion</Text>
+          <Text style={{ color: theme.text }}>Tu ubicacion</Text>
           <Pressable onPress={() => setControler('startPoint')}>
-            <Text style = { styles.ubi} >{startPoint}</Text>
+            <Text style={[styles.ubi, { backgroundColor: theme.subtleBackground }, { color: theme.text }]}>
+              {startPoint}
+            </Text>
           </Pressable>
         
-          <Text>Tu destino</Text>
+          <Text style={{ color: theme.text }}>Tu destino</Text>
           <Pressable onPress={() => setControler('endPoint')}>
-            <Text style = { styles.ubi} >{endPoint}</Text>
+            <Text style={[styles.ubi, { backgroundColor: theme.subtleBackground }, { color: theme.text }]}>
+              {endPoint}
+            </Text>
           </Pressable>
 
           <TouchableOpacity onPress={send}>
-            <Text style = { styles.plus } >Aceptar</Text>
+            <Text style={[styles.plus, { backgroundColor: theme.buttonBackground }]}>
+              Aceptar
+            </Text>
           </TouchableOpacity>
 
         </> : controler === 'endPoint' ?
         <>
-          <Text style = {styles.text}>Punto de destino</Text>
-          <Text style = {styles.ubi}>{data.region.longitude}</Text>
-          {/* aqui va el input del lugar de partida y envia los datos a la api con el algoritmo 
-            aqui podriamos usar la api de google la cual me da el nombre de la ubicacion por las coodenadas*/}
-          {/* <Text style = { styles.text } >Punto de destino</Text>
-          <Text style = {styles.ubi}>{data.pointEnd}</Text> */}
-    
-          {/* aqui va el inpunt del lugar de destino y envia los dato a la api con el algoritmo
-            aqui tambien hacemos lo mismo de arriba*/}
+          <Text style={[styles.text, { color: theme.text }]}>Punto de destino</Text>
+          <Text style={[styles.ubi, { backgroundColor: theme.subtleBackground }]}>
+            {data.region.longitude}
+          </Text>
     
           <Pressable onPress={() => pointEnd(data.region)}>
-            <Text style = {styles.button} >Confirmar</Text>
+            <Text style={[styles.button, { backgroundColor: theme.accent }]}>
+              Confirmar
+            </Text>
           </Pressable>
         </> : 
-        <>
-
-        </>
-        }
+        null
+      }
     </View>
   );
+}
 
-};
-
-const styles = StyleSheet.create(
-  {
-    container : {
-      borderTopLeftRadius: 25,
-      borderTopRightRadius: 25,
-      paddingHorizontal: 5,
-      paddingTop: 5,
-      paddingBottom: 25,
-      position : 'absolute',
-      backgroundColor: 'white',
-      width: '100%',
-      height: 'auto',
-      bottom: 0,
-      alignItems: 'center'
-    },
-    text : {
-      margin: 10,
-      textAlign: 'center'
-    },
-    ubi : {
-      width: '80%',
-      backgroundColor: "rgba(169, 169, 169, 0.8)",
-      padding: 2,
-      margin: 2,
-      fontSize: 20,
-      textAlign: 'center',
-      borderRadius: 5
-    },
-    button: {
-      margin: 10,
-      textAlign: 'center',
-      padding: 10,
-      backgroundColor: 'yellow'
-    },
-    plus : {
-      padding: 10,
-      backgroundColor: 'orange',
-      borderRadius: 100,
-      marginTop: 15
-      
-
-    }
-
+const styles = StyleSheet.create({
+  container: {
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingHorizontal: 5,
+    paddingTop: 5,
+    paddingBottom: 25,
+    position: 'absolute',
+    width: '100%',
+    height: 'auto',
+    bottom: 0,
+    alignItems: 'center'
+  },
+  text: {
+    margin: 10,
+    textAlign: 'center'
+  },
+  ubi: {
+    width: '80%',
+    padding: 2,
+    margin: 2,
+    fontSize: 20,
+    textAlign: 'center',
+    borderRadius: 5
+  },
+  button: {
+    margin: 10,
+    textAlign: 'center',
+    padding: 10,
+    borderRadius: 5,
+    color: 'white'
+  },
+  plus: {
+    padding: 10,
+    borderRadius: 100,
+    marginTop: 15,
+    color: 'white',
+    textAlign: 'center',
+    minWidth: 100
   }
-);
+});
